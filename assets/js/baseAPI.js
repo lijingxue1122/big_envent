e0a01a289ec9d534c080da9cc4cc5f0359f62795
李京雪 <lijingxue@MacBook-Pro.local>
2020-08-01 14:36:42 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,8 +5,29 @@ var baseURl = 'http://ajax.frontend.itheima.net'
 $.ajaxPrefilter(function (options) {
     console.log(options);
     options.url = baseURl + options.url
-        .
-    0
 
+    // 2.包含‘/my/‘的请求统一带上请求头
+    if (options.url.indexOf('/my') != -1) {
+        options.headers = {
+
+            Authorization: localStorage.getItem('token') || ''
+        }
+
+    }
+    // 3.身份验证 
+    options.complete = function (res) {
+
+        var data = res.responseJSON
+        console.log(data);
+        console.log(data.status);
+        console.log(data.message);
+
+        if (data.status == 1 && data.message == "身份认证失败！") {
+
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+
+    }
 
 })
---
+++ assets/js/index.js
@@ -0,0 +1,62 @@
+$(function () {
+
+    getUserInfo()
+    var layer = layui.layer
+
+    $('.btnLogout').on('click', function () {
+
+        layer.confirm('您确定退出吗', {
+            icon: 3,
+            title: '退出提示',
+        }, function (index) {
+            layer.close(index)
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        })
+
+
+    })
+
+})
+
+function getUserInfo() {
+
+
+    $.ajax({
+        type: 'get',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取失败')
+            }
+            // 调用用户渲染函数
+            renderuser(res.data)
+
+        },
+
+        // complete: function (res) {
+
+        //     if (res.response.status === 1 || res.response.message === '身份验证失败')「
+
+        //         localStorage.removeItem('token')
+        //     location.href = '/login.html'
+
+        // }
+    })
+}
+
+function renderuser(user) {
+    var uname = user.nickname || user.username
+    $('.welcome').html('欢迎&nbsp;&nbsp' + uname)
+
+    if (user.user_pic !== null) {
+        $('.layui - nav - img ').show().attr('src', user.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        $('.text-avatar').show().html(uname[0].toUpperCase())
+    }
+}
